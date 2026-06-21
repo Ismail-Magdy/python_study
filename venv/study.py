fa3f8ec97c2a8d4ad4a657a37864c37f9063ca8a
Ismail Magdy <ismailmagdy920@gmail.com>
2026-06-21 08:51:08 +0300

    first commit

---
+++ venv/study.py
@@ -0,0 +1,71 @@
+# # important built-in functions
+
+# char_fun=chr(66)
+# ord_fun=ord("V")
+# len_fun=len("ismail")
+
+# print("------------------")
+# print(char_fun)
+# print("------------------")
+# print(ord_fun)
+# print("------------------")
+# print(len_fun)
+# print("------------------")
+
+
+
+# name="ismail"
+# age=21
+# college="Benha University"
+# country="Egypt"
+# gender="male"
+
+# print(f"My name is {name}, I am {age} years old, I study Engineering at {college}, I live in {country} and I am a {gender}")
+
+
+
+
+
+# name=input("Enter Your Name: ")
+# year_of_birth=int(input("Enter Your year of birth : "))
+# age=2026-year_of_birth
+# print(f"Your Name is : {name}, and Your Age is : {age}")
+
+
+
+# type_variable=3.4
+# print(type(type_variable))
+
+
+
+
+# # Some Methods For Strings
+# name = "ismail magdy ismail masoud ebeed"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+# print(name.strip()) # act as trim in dart
+# print(name.rstrip()) # act as trimRight in dart
+# print(name.lstrip()) # act as trimLeft in dart
+
+
+
+# # Method to convert decimal number to binary number
+# number=2
+# binary_number=bin(number)
+# print(binary_number)
+
+
+# nummber=1.7e5
+# print(nummber)
+
+
+
+# name="ismail"
+# print(dir(name)) # to get all methods and attributes of the variable Object type
+
+
+number=True
+number_two=False
+print(number and number_two) # and operator
+print(number or number_two) # or operator
